fdcfdaab2eba73e3692b2c94d686b0bb69b722e0
CodeVerse | Zakir Hussain <139832875+CodeVerse-team@users.noreply.github.com>
2025-08-18 00:43:35 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,22 @@
+
 'use strict';
 
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
 
+navLinks.forEach((link, i) => {
+  link.addEventListener("click", () => {
+    pages.forEach((page, j) => {
+      if (i === j) {
+        page.classList.add("active");
+        navLinks[j].classList.add("active");
+      } else {
+        page.classList.remove("active");
+        navLinks[j].classList.remove("active");
+      }
+    });
+  });
+});
 
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
@@ -129,30 +145,6 @@ for (let i = 0; i < formInputs.length; i++) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
-    }
-
-  });
-}
-
-
-
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
-
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
     }
 
   });
